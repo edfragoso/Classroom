5c1f17c79a17658b129d53a84757884461f22ed1
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-01-19 17:46:51 -0300

    feat: add implement LoginForm

--- src/components/molecules/login-form/login-form.tsx
+++ src/components/molecules/login-form/login-form.tsx
@@ -1,13 +1,25 @@
+import { FormEvent } from "react";
+import { StyledForm, StyledLoginForm } from "./styles";
 
-export function LoginForm(){
-    return(
-        <div>
-            <h2>Login</h2>
-            <form>
-                <input />
-                <input />
-                <button>Entrar</button>
-            </form>
-        </div>
-    )
+export function LoginForm() {
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const loginPayload = {
+      email: e.currentTarget.email.value,
+      password: e.currentTarget.password.value,
+    };
+    console.log(loginPayload)
+  }
+
+  return (
+    <StyledLoginForm>
+      <h2>Login</h2>
+      <StyledForm onSubmit={handleSubmit}>
+        <input placeholder="e-mail" name="email" />
+        <input placeholder="Senha" name="password" />
+        <button type="submit">Entrar</button>
+      </StyledForm>
+    </StyledLoginForm>
+  );
 }
