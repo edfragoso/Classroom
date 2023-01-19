fd27d68c0751a140efff30a2500dc3aa34067683
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-01-19 17:48:04 -0300

    feat: add login

---
+++ src/components/pages/login/login.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "../../molecules/login-form/login-form";
+import { StyledLoginPage } from "./styles";
+
+export function Login() {
+  
+    return (
+    <StyledLoginPage>
+      <LoginForm />
+    </StyledLoginPage>
+  );
+}
